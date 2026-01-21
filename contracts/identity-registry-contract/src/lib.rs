f1496ef14d6d0b6d543cad3c01a3243e1debe417
Hakeem Kazeem <hakymulla@gmail.com>
2026-01-21 19:36:48 +0100

    batch verification implementation

--- contracts/identity-registry-contract/src/lib.rs
+++ contracts/identity-registry-contract/src/lib.rs
@@ -8,8 +8,10 @@ mod types;
 #[cfg(test)]
 mod test;
 
-use soroban_sdk::{contract, contractimpl, Address, Env};
-use crate::error::RegistryError;
+use core::ops::Add;
+
+use soroban_sdk::{contract, contractimpl, Address, Env, Vec};
+use crate::{error::RegistryError, types::ExpertStatus};
 
 #[contract]
 pub struct IdentityRegistryContract;
@@ -20,4 +22,25 @@ impl IdentityRegistryContract {
     pub fn init(env: Env, admin: Address) -> Result<(), RegistryError> {
         contract::initialize_registry(&env, &admin)
     }
-}
+
+    /// Batch Verification
+    pub fn batch_add_experts(env:Env, experts: Vec<Address>) -> Result<(), RegistryError> {
+        if experts.len() > 20 {
+            return Err(RegistryError::ExpertVecMax);
+        }
+
+        let admin = storage::get_admin(&env).unwrap();
+        admin.require_auth();
+
+        for expert in experts {
+            let status = storage::get_expert_status(&env, &expert);
+            if status == ExpertStatus::Verified {
+                return Err(RegistryError::AlreadyVerified);
+            }
+            storage::set_expert_record(&env, &expert, ExpertStatus::Verified);
+            events::emit_status_change(&env, expert, ExpertStatus::Unverified, ExpertStatus::Verified, admin.clone());
+        }
+        
+        Ok(())
+    }
+} 
--- contracts/identity-registry-contract/src/test.rs
+++ contracts/identity-registry-contract/src/test.rs
@@ -1,6 +1,7 @@
 #![cfg(test)]
 use crate::{IdentityRegistryContract, IdentityRegistryContractClient};
-use soroban_sdk::{Env, testutils::Address as _};
+use soroban_sdk::{Env, Vec, testutils::Address as _, Address, vec};
+use crate::{storage, types::ExpertStatus};
 
 #[test]
 fn test_initialization() {
@@ -18,4 +19,92 @@ fn test_initialization() {
     // 3. Call init again (Should fail)
     let res_duplicate = client.try_init(&admin);
     assert!(res_duplicate.is_err());
+}
+
+#[test]
+fn test_batch_verification() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, IdentityRegistryContract);
+    let client = IdentityRegistryContractClient::new(&env, &contract_id);
+
+    let admin = soroban_sdk::Address::generate(&env);
+    client.init(&admin);
+
+    let experts = vec![&env, Address::generate(&env), Address::generate(&env), Address::generate(&env)];
+
+    client.batch_add_experts(&experts);
+}
+
+#[test]
+#[should_panic]
+fn test_batch_verification_no_admin() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, IdentityRegistryContract);
+    let client = IdentityRegistryContractClient::new(&env, &contract_id);
+
+    let experts = vec![&env, Address::generate(&env), Address::generate(&env), Address::generate(&env)];
+
+    client.batch_add_experts(&experts);
+}
+
+#[test]
+fn test_batch_verification_check_status() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, IdentityRegistryContract);
+    let client = IdentityRegistryContractClient::new(&env, &contract_id);
+
+    let admin = soroban_sdk::Address::generate(&env);
+    client.init(&admin);
+
+    let e1 = Address::generate(&env);
+    let e2 = Address::generate(&env);
+    let e3 = Address::generate(&env);
+    let e4 = Address::generate(&env);
+    let e5 = Address::generate(&env);
+
+    let experts = vec![&env, e1.clone(), e2.clone(), e3.clone(), e4.clone(), e5.clone()];
+
+    client.batch_add_experts(&experts);
+
+    env.as_contract(&contract_id, ||{
+        assert_eq!(storage::get_expert_status(&env, &e1), ExpertStatus::Verified);
+        assert_eq!(storage::get_expert_status(&env, &e2), ExpertStatus::Verified);
+        assert_eq!(storage::get_expert_status(&env, &e3), ExpertStatus::Verified);
+        assert_eq!(storage::get_expert_status(&env, &e4), ExpertStatus::Verified);
+        assert_eq!(storage::get_expert_status(&env, &e5), ExpertStatus::Verified);
+    })
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #7)")]
+fn test_batch_verification_max_vec() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, IdentityRegistryContract);
+    let client = IdentityRegistryContractClient::new(&env, &contract_id);
+
+    let admin = soroban_sdk::Address::generate(&env);
+    client.init(&admin);
+
+    let e1 = Address::generate(&env);
+    let e2 = Address::generate(&env);
+    let e3 = Address::generate(&env);
+    let e4 = Address::generate(&env);
+
+    let experts = vec![&env, e1.clone(), e2.clone(), e3.clone(), e4.clone(), 
+        e1.clone(), e2.clone(), e3.clone(), e4.clone(), 
+        e1.clone(), e2.clone(), e3.clone(), e4.clone(), 
+        e1.clone(), e2.clone(), e3.clone(), e4.clone(),
+        e1.clone(), e2.clone(), e3.clone(), e4.clone(),
+        e1.clone(), e2.clone(), e3.clone(), e4.clone()
+    ];
+
+    client.batch_add_experts(&experts);
 }
